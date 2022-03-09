7ca6ab5a11d9bf98cc23b14a9440f45e55e7b296
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-03-09 15:17:11 +0330

    remove button works

--- memorycard.js
+++ memorycard.js
@@ -6,122 +6,102 @@ const cards = [
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.question}</div>
     </div>`},
     cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.answer}</div>
     </div>`}
   },
   {
     question: "Demo question 2",
     answer: "Demo answer 2",
-    cardElementQuestion:function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
+    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.question}</div>
     </div>`},
-    cardElementAnswer: function(){return `<div onclick="showAnswer(event)" class="col gy-4">
+    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.answer}</div>
     </div>`}
   },
   {
     question: "Demo question 3",
     answer: "Demo answer 3",
-    cardElementQuestion:function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
+    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.question}</div>
     </div>`},
-    cardElementAnswer: function(){return `<div onclick="showAnswer(event)" class="col gy-4">
+    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
     <div class="p-4 border bg-light" id="my-card">
       <div id="icon">
         <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`}
-  },
-  {
-    question: "Demo question 4",
-    answer: "Demo answer 4",
-    cardElementQuestion:function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function(){return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`}
-  },
-  {
-    question: "Demo question 5",
-    answer: "Demo answer 5",
-    cardElementQuestion:function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function(){return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i class="fas fa-regular fa-xmark"></i>
+        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
       </div>${this.answer}</div>
     </div>`}
   },
 ];
 
 let showAnswer = function(event){
-    var target;
+    var tar;
     var question = true;
     for(let i=0; i<cards.length; i++){
         if (cards[i].cardElementQuestion().slice(55, -11)==event.target.outerHTML) {
-            target = i // cards[target].cardElementAnswer()
+            tar = i // cards[target].cardElementAnswer()
         } else if (cards[i].cardElementAnswer().slice(55, -11)==event.target.outerHTML) {
-            target = i;
+            tar = i;
             question = false;
         }
     }
     if(question){
-        event.target.outerHTML = cards[target].cardElementAnswer()
+        event.target.outerHTML = cards[tar].cardElementAnswer()
     } else {
-        event.target.outerHTML = cards[target].cardElementQuestion()
+        event.target.outerHTML = cards[tar].cardElementQuestion()
     }
     
 }
 
-//let cardColumn = `<div class="row gx-5">${cardElement}${cardElement}</div>`;
-var element = "";
-
-for (let i = 0; i < cards.length; i++) {
-  if (i%2==0){
-    element += `<div class="row gx-5">${cards[i].cardElementQuestion()}</div>`
-  } else if (i%2==1) {
-      element = element.slice(0, -6)
-      element += cards[i].cardElementQuestion() + '</div>'
-  }
+var remove = function(event){
+    let text = event.path[2]['innerText'];
+    for (let i=0; i<cards.length; i++){
+        if (cards[i].question==text || cards[i].answer==text){
+            cards.splice(i,1)
+            break;
+        }
+    }
+    start();
 }
 
-document.getElementById('container').innerHTML = element;
+
+var start = function() {
+  var element = "";
+  for (let i = 0; i < cards.length; i++) {
+    if (i%2==0){
+        element += `<div class="row gx-5">${cards[i].cardElementQuestion()}</div>`
+    } else if (i%2==1) {
+        element = element.slice(0, -6)
+        element += cards[i].cardElementQuestion() + '</div>'
+    }
+}
+if(element.length==0){
+    document.getElementById('container').innerHTML = '<h1>No Cards</h1>'
+} else {
+    document.getElementById('container').innerHTML = element;
+}
+}
+
 
 
