49bd99567b120b200ac4ef64524e617c9044dcd1
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-03-10 14:35:39 +0330

    show answers and questions
    showAnswer works, also changed the design

--- memorycard.js
+++ memorycard.js
@@ -1,81 +1,95 @@
-const cards = [
-  {
-    question: "Demo question",
-    answer: "Demo answer",
-    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`}
-  },
-  {
-    question: "Demo question 2",
-    answer: "Demo answer 2",
-    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`}
-  },
-  {
-    question: "Demo question 3",
-    answer: "Demo answer 3",
-    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`}
-  },
-];
+const card = {
+  question: "Demo question",
+  answer: "Demo answer",
+  cardElementQuestion: function(){ 
+    return `<div class="col gy-4">
+  <div class="p-4 border bg-light" id="my-card">
+    <div id="icon">
+      <i onclick="setModalText(event)" data-bs-toggle="modal" data-bs-target="#editModal" class="fa-solid fa-pen-to-square"></i>
+      <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
+    </div>
+    <div id="q">${this.question}</div>
+    <div id="edit" onclick="showAnswer(event)">
+    <span id="c-ans" >See Answer</span>
+    <i class="fa-solid fa-angles-left"></i>
+    </div>
+  </div>
+  </div>`}
+}
 
-let showAnswer = function(event){
-    var tar;
-    var question = true;
-    for(let i=0; i<cards.length; i++){
-        if (cards[i].cardElementQuestion().slice(55, -11)==event.target.outerHTML) {
-            tar = i // cards[target].cardElementAnswer()
-        } else if (cards[i].cardElementAnswer().slice(55, -11)==event.target.outerHTML) {
-            tar = i;
-            question = false;
+const cards = [card];
+
+var cardNum;
+
+var setModalText = function(event){
+    let text = event.path[2]['innerText'];
+    for (let i=0; i<cards.length; i++){
+        if(text==cards[i].question){
+            let q = cards[i].question;
+            let a = cards[i].answer;
+            document.getElementById('modalQuestion').innerHTML = q;
+            document.getElementById('modalAnswer').innerHTML = a;
+            cardNum = i;
         }
     }
-    if(question){
-        event.target.outerHTML = cards[tar].cardElementAnswer();
-    } else {
-        event.target.outerHTML = cards[tar].cardElementQuestion();
+}
+
+var editCard = function(event){
+    event.preventDefault();
+    let questionEdited = event.target.form[0].value;
+    let answerEdited = event.target.form[1].value;
+
+    cards[cardNum].question = questionEdited;
+    document.getElementById('modalQuestion').innerHTML = '';
+    cards[cardNum].answer = answerEdited;
+    document.getElementById('modalAnswer').innerHTML = '';
+    questionEdited='';
+    answerEdited='';
+    start();
+    $('#editModal').modal('toggle');
+}
+
+var showAnswer = function(event){
+  let target;
+  let target2;
+  var isQuestion = true;
+  var card;
+  let q = event.path[2].children[1].innerHTML;
+  // find the card in cards array >> cards[target]
+  for (let i=0; i<cards.length; i++){
+    if (cards[i].question==q){
+      target = i;
+    } else if(cards[i].answer==q){
+      target = i;
+      isQuestion = false;
     }
-    
+  }
+  // find the element in document >> document.querySelectorAll('#q')[target2]
+  let elements = document.querySelectorAll('#q')
+  for (let i=0; i<elements.length; i++){
+    if(elements[i].innerHTML==q){
+      target2 = i
+    }
+  }
+  // find the card that was clicked >> document.querySelectorAll('#my-card')[card].children[2].children[0]
+  let myCards = document.querySelectorAll('#my-card');
+  for (let i=0; i<myCards.length; i++){
+    if (myCards[i].children[1].innerHTML==q){
+      card = i;
+    }
+  }
+  // show the answer or question based on status
+  if (isQuestion){
+    document.querySelectorAll('#q')[target2].innerHTML = cards[target].answer;
+    document.querySelectorAll('#my-card')[card].children[2].children[0].innerHTML = 'See Question';
+  } else {
+    document.querySelectorAll('#q')[target2].innerHTML = cards[target].question;
+    document.querySelectorAll('#my-card')[card].children[2].children[0].innerHTML = 'See Answer';
+  }
 }
 
 var remove = function(event){
-    let text = event.path[2]['innerText'];
+    let text = event.path[2].children[1].innerHTML;
     for (let i=0; i<cards.length; i++){
         if (cards[i].question==text || cards[i].answer==text){
             cards.splice(i,1)
@@ -91,31 +105,31 @@ var hideDone = function(){
 
 var addCard = function(event){
     event.preventDefault();
-    let question = document.getElementsByName('question')[0].value;
+    let ques = document.getElementsByName('question')[0].value;
     document.getElementsByName('question')[0].value = '';
-    let answer = document.getElementsByName('answer')[0].value;
+    let ans = document.getElementsByName('answer')[0].value;
     document.getElementsByName('answer')[0].value = '';
-    if (question=='' || answer==''){
+    if (ques=='' || ans==''){
         document.getElementById('info').innerHTML = 'Write somthing then try again!';
     } else {
         cards.push({
-            question: "Demo question",
-    answer: "Demo answer",
-    cardElementQuestion: function(){ return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.question}</div>
-    </div>`},
-    cardElementAnswer: function() { return `<div onclick="showAnswer(event)" class="col gy-4">
-    <div class="p-4 border bg-light" id="my-card">
-      <div id="icon">
-        <i class="fa-solid fa-pen-to-square"></i>
-        <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
-      </div>${this.answer}</div>
-    </div>`
-        }});
+          question: ques,
+          answer: ans,
+          cardElementQuestion: function(){ 
+            return `<div class="col gy-4">
+          <div class="p-4 border bg-light" id="my-card">
+            <div id="icon">
+              <i onclick="setModalText(event)" data-bs-toggle="modal" data-bs-target="#editModal" class="fa-solid fa-pen-to-square"></i>
+              <i onclick="remove(event)" class="fas fa-regular fa-xmark"></i>
+            </div>
+            <div id="q">${this.question}</div>
+            <div id="edit" onclick="showAnswer(event)">
+            <span id="c-ans" >See Answer</span>
+            <i class="fa-solid fa-angles-left"></i>
+            </div>
+          </div>
+          </div>`}
+        });
         document.getElementById('done').style.display = 'block';
         document.getElementById('info').innerHTML = ''
         setTimeout(hideDone,3000)
